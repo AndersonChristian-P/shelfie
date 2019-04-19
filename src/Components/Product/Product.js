816edb516f7e0915270e207085e0d68df23ba0d4
Christian Anderson <ca96187@gmail.com>
2019-04-19 11:06:35 -0600

    completed Design Step 1

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -5,14 +5,59 @@ export default class Product extends Component {
     super()
 
     this.state = {
-
+      imageUrl: "",
+      product_name: "",
+      price: 0
     }
+  }
+
+  handleChange = (event) => {
+    let { value, name } = event.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleClickCancel = () => {
+    this.setState({
+      imageUrl: "",
+      product_name: "",
+      price: 0
+    })
   }
 
   render() {
     return (
       <div>
-        Product
+        <div>Image URL:
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={this.state.imageUrl}
+            name="imageUrl"
+          />
+        </div>
+
+        <div>Product Name:
+          <input
+            onChange={this.handleChange}
+            type="text"
+            value={this.state.product_name}
+            name="product_name"
+          />
+        </div>
+
+        <div>Price:
+          <input
+            onChange={this.handleChange}
+            type="number"
+            value={this.state.price}
+            name="price"
+          />
+        </div>
+
+        <button onClick={this.handleClickCancel}>Cancel</button>
+        <button>Add to Inventory</button>
       </div>
     )
   }
