aeb3198fb59ced1f2c83f03ab5ffb811c3716d7b
Christian Anderson <ca96187@gmail.com>
2019-04-19 10:39:14 -0600

    setup database

---
+++ db/create_product_table.sql
@@ -0,0 +1,6 @@
+create table product (
+  product_id serial primary key,
+  name varchar(25),
+  price integer,
+  img text
+);
--- server/index.js
+++ server/index.js
@@ -1,15 +1,21 @@
+require("dotenv").config()
 const express = require("express")
+const massive = require("massive")
+
+const { SERVER_PORT, CONNECTION_STRING } = process.env
 
 const app = express()
-const port = 5356
 
 const productCtrl = require("./controller")
 
 app.use(express.json())
 
-
-
-app.listen(port, () => {
-  console.log(`listening on port ${port}`)
+massive(CONNECTION_STRING).then(dbInstance => {
+  app.set("db", dbInstance)
+  app.listen(SERVER_PORT, () => {
+    console.log(`listening on port ${SERVER_PORT}`)
+  })
 })
-
+  .catch(err => {
+    console.log(`massive err ${err}`)
+  })
